88dc8703c86b7e01ae54131c6a589f6e06f18bb0
Mackewinsson P <mackewinssonp.@MacBook-Pro-de-Mackewinsson.local>
2021-06-28 11:36:39 -0400

    featured modalidad added2

--- components/common/Modal.js
+++ components/common/Modal.js
@@ -105,17 +105,6 @@ const Modal = ({ show, onClose, children, title, data }) => {
   }
 };
 
-const Featured = styled.div`
-  background-color: #2997ff;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  border-radius: 25px 25px 0px 0px;
-  font-size: 0.7em;
-  font-weight: normal;
-  padding: 2px 0px;
-`;
-
 const GiftWrapper = styled.div`
   display: flex;
   width: 100%;
