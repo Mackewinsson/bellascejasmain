bbbcc717529ade544b31c377b9d8d28ec9cd542c
mackewinsson <mackewinsson@gmail.com>
2022-02-09 23:55:22 -0300

    partial config of firebase auth

---
+++ firebase/firebaseAuth.js
@@ -0,0 +1,19 @@
+import Firebase from "Firebase/app";
+import "Firebase/auth";
+
+const FirebaseCredentials = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+// if a Firebase instance doesn't exist, create one
+if (!Firebase.apps.length) {
+  Firebase.initializeApp(FirebaseCredentials);
+}
+
+export default Firebase;
